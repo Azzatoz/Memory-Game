87cfb5a8947eb455779002ba4e83d31864981033
Azzatoz <83659233+Azzatoz@users.noreply.github.com>
2023-12-24 11:50:44 +0300

    # old data Card

--- app/src/main/java/com/example/testtask/ui/game/GridItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.testtask.ui.game
-
-data class GridItem(val id: Int, val imageResId: Int, var isFlipped: Boolean = false) {
-    fun flip() {
-        isFlipped = !isFlipped
-    }
-}
